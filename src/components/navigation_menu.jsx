62d107efe8e137e08e4618b3fff263598e30c4ce
Lukáš Prchlík <prchlik1819@gmail.com>
2023-12-13 11:06:47 +0100

    test

---
+++ src/components/navigation_menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavigationMenu() {
+    return (
+        <ul>
+            <li><a href="#section1">Sekce 1</a></li>
+            <li><a href="#section2">Sekce 2</a></li>
+            <li><a href="#section3">Sekce 3</a></li>
+        </ul>
+    );
+}
+
+export default NavigationMenu;
