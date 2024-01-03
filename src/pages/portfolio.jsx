6c314223a5ff287257430a6cb0f9efb3ac775b43
wannabe20 <prchlik1819@gmail.com>
2024-01-03 11:07:09 +0100

    pokus

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import NavScroll from '../components/header/navbar/navbar';
 import Footer from '../components/header/footer/footer';
 import {introdata} from '../Metadata'
+import Component from '../smazat';
 
 function Portfolio() {
     return (
@@ -13,16 +14,9 @@ function Portfolio() {
                 <h2>
                     <introdata className="title"></introdata>
                 </h2>
-                
+                <Component></Component>
             </section>
-            <section id="section2">
-                <h2>Sekce 2</h2>
-                
-            </section>
-            <section id="section3">
-                <h2>Sekce 3</h2>
-                
-            </section>
+            
             <Footer></Footer>
         </div>
     );
